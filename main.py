589b7789ff70ecdcfd19b289b3eaf4dc2c43cbc3
Hidayatullah <100272473+dayat123123@users.noreply.github.com>
2022-12-15 00:47:40 +0700

    bismillah

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from googletrans import Translator
 translator = Translator(service_urls=['translate.googleapis.com'])
 import os
