d6c1f56bad98589e31e12dab62c6f01f3ab542eb
Hidayatullah <100272473+dayat123123@users.noreply.github.com>
2022-12-15 01:01:20 +0700

    ss

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from googletrans import Translator
 translator = Translator(service_urls=['translate.googleapis.com'])
 import os
 import mysql.connector
-app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net'
-app.config['MYSQL_USER'] = 'sql6513279'
-app.config['MYSQL_PASSWORD'] = '71TDCiXpb4'
-app.config['MYSQL_DB'] = 'sql6513279'
-mysql = MySQL(app)
+# app = Flask(__name__)
+# app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net'
+# app.config['MYSQL_USER'] = 'sql6513279'
+# app.config['MYSQL_PASSWORD'] = '71TDCiXpb4'
+# app.config['MYSQL_DB'] = 'sql6513279'
+# mysql = MySQL(app)
 
 # route api
 @app.route('/')
@@ -29,28 +29,28 @@ def returnvalue():
     return d
 
 
-@app.route('/api8', methods = ['GET'])
-def returnvalue8():
-    global banj
-    new_string = {}
-    my_list = []
-    d= {}
-    string = str(request.args['query'])
-    list_string = string.split()
-    n = len(list_string)
-    for i in range(n):
-        cur = mysql.connection.cursor()
-        row_count = cur.execute("SELECT kata_dasar FROM tb_katadasar2 where kata_daerah = %s", [list_string[i]])
-        mysql.connection.commit()
-        if row_count > 0:
-            banj=cur.fetchone()[0]
-            answer = banj
-            my_list.append(answer)
-        else:
-            my_list.append(list_string[i])
+# @app.route('/api8', methods = ['GET'])
+# def returnvalue8():
+#     global banj
+#     new_string = {}
+#     my_list = []
+#     d= {}
+#     string = str(request.args['query'])
+#     list_string = string.split()
+#     n = len(list_string)
+#     for i in range(n):
+#         cur = mysql.connection.cursor()
+#         row_count = cur.execute("SELECT kata_dasar FROM tb_katadasar2 where kata_daerah = %s", [list_string[i]])
+#         mysql.connection.commit()
+#         if row_count > 0:
+#             banj=cur.fetchone()[0]
+#             answer = banj
+#             my_list.append(answer)
+#         else:
+#             my_list.append(list_string[i])
 
-    d['output'] = ' '.join(my_list)
-    return d
+#     d['output'] = ' '.join(my_list)
+#     return d
 # tutup route api
 
 
