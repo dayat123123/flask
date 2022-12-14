a259c6a36ba86e9b0f371c6e57b8e6f9596f0172
Hidayatullah <100272473+dayat123123@users.noreply.github.com>
2022-12-15 00:57:26 +0700

    tambah route banjar 8

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app.config['MYSQL_PASSWORD'] = '71TDCiXpb4'
 app.config['MYSQL_DB'] = 'sql6513279'
 mysql = MySQL(app)
 
+# route api
 @app.route('/')
 def index():
     aa = "Saya sangat senang karena program saya berjalan, Alhamdulillah ya Allah"
@@ -26,6 +27,31 @@ def returnvalue():
     answer = hasilbanjar.text
     d['output'] = answer
     return d
+
+
+@app.route('/api8', methods = ['GET'])
+def returnvalue8():
+    global banj
+    new_string = {}
+    my_list = []
+    d= {}
+    string = str(request.args['query'])
+    list_string = string.split()
+    n = len(list_string)
+    for i in range(n):
+        cur = mysql.connection.cursor()
+        row_count = cur.execute("SELECT kata_dasar FROM tb_katadasar2 where kata_daerah = %s", [list_string[i]])
+        mysql.connection.commit()
+        if row_count > 0:
+            banj=cur.fetchone()[0]
+            answer = banj
+            my_list.append(answer)
+        else:
+            my_list.append(list_string[i])
+
+    d['output'] = ' '.join(my_list)
+    return d
+# tutup route api
 
 
 if __name__ == '__main__':
